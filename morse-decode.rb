c904b4f294b49e1fb3237ac271e606f14aed8292
Erwin Amador <eamador0305@gmail.com>
2023-11-16 16:36:10 -0600

    add space between blocks

--- morse-decode.rb
+++ morse-decode.rb
@@ -4,7 +4,9 @@ morse_to_letter = { ".-" => "a", "-..." => "b", "-.-." => "c", "-.." => "d", "."
  "--." => "g", "...." => "h", ".." => "i", ".---" => "j", "-.-" => "k", ".-.." => "l", "--" => "m", 
  "-." => "n", "---" => "o",  ".--." => "p", "--.-" => "q", ".-." => "r", "..." => "s", "-" => "t", 
  "..-" => "u", "...-" => "v", ".--" => "w", "-..-" => "x", "-.--" => "y", "--.." => "z"}
+
 words = code.split("   ")
+
 decoded_message = words.map { |word|
     letters = word.split(" ")
     letters.map { |letter| morse_to_letter[letter] }.join
